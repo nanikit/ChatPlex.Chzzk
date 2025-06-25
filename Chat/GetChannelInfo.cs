5a7ea1b3c1b7558e5f9e06637d25dca0d7176cc5
nanikit <nanikit4@gmail.com>
2025-06-26 02:41:40 +0900

    feat: add edge cases

--- Chat/GetChannelInfo.cs
+++ Chat/GetChannelInfo.cs
@@ -7,9 +7,12 @@ using System.Threading.Tasks;
 
 namespace ChatPlex.Chzzk.Chat
 {
-  record class ChatChannelAccess(string Id);
-  record class DeadChannel(string Id) : ChatChannelAccess(Id);
-  record class LiveChannel(string Id, string AccessToken, string ExtraToken) : ChatChannelAccess(Id);
+  record class ChatChannelAccess();
+  record class NotFoundChannel() : ChatChannelAccess();
+  record class NotCreatedChannel() : ChatChannelAccess();
+  record class LimitedChannel() : ChatChannelAccess();
+  record class DeadChannel() : ChatChannelAccess();
+  record class LiveChannel(string Id, string AccessToken, string ExtraToken) : ChatChannelAccess();
 
   class GetChannelInfo
   {
@@ -20,12 +23,27 @@ namespace ChatPlex.Chzzk.Chat
         cancellationToken.ThrowIfCancellationRequested();
 
         var channel = await GetChannelAccess(cancellationToken).ConfigureAwait(false);
-        if (channel is LiveChannel liveChannel)
+        switch (channel)
         {
-          return liveChannel;
+          case LiveChannel liveChannel:
+            return liveChannel;
+          case NotFoundChannel:
+            Plugin.Log?.Warn($"Channel ID({PluginConfig.Instance.ChannelId}) is not found. Please check your configuration.");
+            break;
+          case DeadChannel:
+            Plugin.Log?.Info("Channel is not live. Waiting for 10 seconds...");
+            break;
+          case NotCreatedChannel:
+            Plugin.Log?.Warn("Have you ever streamed on Chzzk? Please stream first.");
+            break;
+          case LimitedChannel:
+            Plugin.Log?.Warn("Cannot get channel ID. Are you streaming for adult only?");
+            retryCount++;
+            break;
+          default:
+            break;
         }
 
-        Plugin.Log?.Info("Channel is not live. Waiting for 10 seconds...");
         await Task.Delay(10000, cancellationToken).ConfigureAwait(false);
       }
     }
@@ -43,21 +61,34 @@ namespace ChatPlex.Chzzk.Chat
       Plugin.Log?.Debug($"{typeof(GetChannelInfo).Name}: SendRequest()");
 
       // Get Channel Information
-      string channelResponse = await client.DownloadStringTaskAsync($"https://api.chzzk.naver.com/polling/v2/channels/{channelId}/live-status").ConfigureAwait(false);
-      if (cancellationToken.IsCancellationRequested)
+      string channelResponse;
+      try
       {
-        return null;
+        channelResponse = await client.DownloadStringTaskAsync($"https://api.chzzk.naver.com/polling/v3.1/channels/{channelId}/live-status").ConfigureAwait(false);
       }
+      catch (WebException exception)
+      {
+        if (exception.Message.Contains("404"))
+        {
+          return new NotFoundChannel();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      cancellationToken.ThrowIfCancellationRequested();
 
       JObject liveStatus = JObject.Parse(channelResponse);
-      string? chatChannelId = liveStatus["content"]?["chatChannelId"]?.Value<string>();
-      if (chatChannelId == null)
+
+      var content = liveStatus["content"];
+      if (content == null)
       {
-        Plugin.Log?.Error("Channel Info Access Fail! - Chat Channel Id is null");
-        return null;
+        return new NotCreatedChannel();
       }
 
-      string? livePollingStatusJson = liveStatus["content"]?["livePollingStatusJson"]?.Value<string>();
+      string? livePollingStatusJson = content["livePollingStatusJson"]?.Value<string>();
       if (livePollingStatusJson == null)
       {
         Plugin.Log?.Warn("Channel Info Access Fail! - livePollingStatusJson is null");
@@ -66,10 +97,16 @@ namespace ChatPlex.Chzzk.Chat
       {
         JObject livePollingStatus = JObject.Parse(livePollingStatusJson);
         string? status = livePollingStatus["status"]?.Value<string>();
-        if (status == "STOPPED")
+        if (status != "STARTED")
         {
-          return new DeadChannel(chatChannelId);
+          return new DeadChannel();
         }
+      }
+
+      string? chatChannelId = content["chatChannelId"]?.Value<string>();
+      if (chatChannelId == null)
+      {
+        return new LimitedChannel();
       }
 
       Plugin.Log?.Info($"{typeof(GetChannelInfo).Name}: SendRequest() - Chat Channel Id: {chatChannelId}");
@@ -77,9 +114,9 @@ namespace ChatPlex.Chzzk.Chat
       // Get Access Token
       string tokenResponse = await client.DownloadStringTaskAsync($"https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId={chatChannelId}&chatType=STREAMING").ConfigureAwait(false);
       JObject tokenJson = JObject.Parse(tokenResponse);
-      var content = tokenJson["content"];
-      string? accessToken = content?["accessToken"]?.Value<string>();
-      string? extraToken = content?["extraToken"]?.Value<string>();
+      var tokenContent = tokenJson["content"];
+      string? accessToken = tokenContent?["accessToken"]?.Value<string>();
+      string? extraToken = tokenContent?["extraToken"]?.Value<string>();
 
       Plugin.Log?.Info($"{typeof(GetChannelInfo).Name}: Access: {accessToken}, Extra: {extraToken}");
       if (accessToken == null || extraToken == null)
